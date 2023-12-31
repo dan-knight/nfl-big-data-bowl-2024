086153098e76f95e7fe892c7a34abf18be3617e2
dan-knight <dan@danknight.dev>
2023-12-31 14:36:09 -0800

    GIF animation render utility function

--- nfl_bdb/visualize/__init__.py
+++ nfl_bdb/visualize/__init__.py
@@ -4,3 +4,14 @@ from matplotlib.animation import Animation
 
 def render_animation_video(animation: Animation):
     return HTML(animation.to_html5_video())
+
+
+def render_animation_image(animation: Animation):
+    writer = PillowWriter(
+        fps=24,
+        bitrate=1800
+    )
+
+    with NamedTemporaryFile(suffix='.gif') as temp:
+        animation.save(temp.name, writer=writer)
+        gif = Image(data=open(temp.name,'rb').read(), format='gif')
